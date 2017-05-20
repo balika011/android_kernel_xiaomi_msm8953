865e4b8ec34d55fa6639aa66e65f151dbc843d11
Balázs Triszka <balika011@gmail.com>
2017-05-20 20:24:00 +0200

    mido: irda
    Signed-off-by: Balázs Triszka <balika011@gmail.com>

--- drivers/media/rc/rc-main.c
+++ drivers/media/rc/rc-main.c
@@ -737,8 +737,17 @@ EXPORT_SYMBOL_GPL(rc_open);
 static int ir_open(struct input_dev *idev)
 {
 	struct rc_dev *rdev = input_get_drvdata(idev);
+	int rc = 0;
 
-	return rc_open(rdev);
+	mutex_lock(&rdev->lock);
+
+	rc = rdev->open(rdev);
+	if (rc >= 0)
+		rdev->open_count++;
+
+	mutex_unlock(&rdev->lock);
+
+	return rc;
 }
 
 void rc_close(struct rc_dev *rdev)
@@ -757,7 +766,13 @@ EXPORT_SYMBOL_GPL(rc_close);
 static void ir_close(struct input_dev *idev)
 {
 	struct rc_dev *rdev = input_get_drvdata(idev);
-	rc_close(rdev);
+
+	 if (rdev) {
+		mutex_lock(&rdev->lock);
+		if (!--rdev->open_count)
+			rdev->close(rdev);
+		mutex_unlock(&rdev->lock);
+	}
 }
 
 /* class for /sys/class/rc */
